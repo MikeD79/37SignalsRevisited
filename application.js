b203d1d71c058db753c304e5bdb5e88486f83d3b
Michael Daniel <mpdaniel79@gmail.com>
2013-10-16 21:26:02 -0400

    Adds jQuery functionality for all products

--- application.js
+++ application.js
@@ -2,10 +2,30 @@ $(document).ready(function(){
     $("#bc,#hr,#cf,.arrow").hide();
     
     $("#Basecamp").on("mouseenter",function() {
-    
         $("#bc, #Basecamp .arrow").show();
         $("#general").hide();
-    
-    });
-    
+        });
+
+    $("#Basecamp").on("mouseleave", function() {
+    	$("#bc, #Basecamp .arrow").hide();
+        $("#general").show();
+    	});
+    $("#Highrise").on("mouseenter",function() {
+        $("#bc, #Highrise .arrow").show();
+        $("#general").hide();
+        });
+
+    $("#Highrise").on("mouseleave", function() {
+    	$("#bc, #Highrise .arrow").hide();
+        $("#general").show();
+    	});
+    $("#Campfire").on("mouseenter",function() {
+        $("#bc, #Campfire .arrow").show();
+        $("#general").hide();
+        });
+
+    $("#Campfire").on("mouseleave", function() {
+    	$("#bc, #Campfire .arrow").hide();
+        $("#general").show();
+    	});
   });
